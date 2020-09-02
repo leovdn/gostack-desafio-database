c26ce38d58ad1ea705039a1f799a0ed6324b19f4
leovdn <leo_vdn@hotmail.com>
2020-09-02 02:48:03 -0300

    created transactions and categories database migrations

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,64 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Transaction type Invalid.');
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance.');
+    }
+
+    let categoryOnTable = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryOnTable) {
+      categoryOnTable = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryOnTable);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryOnTable,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
